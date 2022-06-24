24c8422535e8ab6a2404002829f79cb87bed7ba0
masatoshi kusano <uppleate@cordiary.net>
2022-06-24 10:16:29 +0900

    オブジェクト配列の定義方法を修正、キーとタスクの名前を変更、addTask関数にコンソール表示の処理を追加

--- kadai①-4.js
+++ kadai①-4.js
@@ -3,44 +3,43 @@ const title =
 現在持っているタスクの一覧
 ===========================`;
 
-const cleaning = {
-  action:'机を片付ける',
-  genre:'掃除'
-};
-
-const shopping = {
-  action:'牛乳を買う',
-  genre:'買い物'
-};
-
-const motion = {
-  action:'散歩する',
-  genre:'運動'
-};
-
-const taskList = [cleaning, shopping, motion];
+const tasks = [
+  {
+    name:'机を片付ける',
+    genre:'掃除'
+  },
+  {
+    name:'牛乳を買う',
+    genre:'買い物'
+  },
+  {
+    name:'散歩する',
+    genre:'運動'
+  }
+];
 
 const displayTaskList = () => {
   console.log(title);
-  taskList.forEach((value, index) => {
-    console.log(`${index}：[内容]${taskList[index].action}、[ジャンル]${taskList[index].genre}`);
+  tasks.forEach((task, index) => {
+    console.log(`${index}：[内容]${tasks[index].name}、[ジャンル]${tasks[index].genre}`);
   });
 }
 
-function task(action, genre) {
-  this.action = action;
-  this.genre = genre;
+function CreateTask(name, genre) {
+  return{
+    name,
+    genre
+  }
 }
 
 const addTask = (addAction, addGenre) => {
-  const newTask = new task(addAction, addGenre);
-  taskList.push(newTask);
+  tasks.push(CreateTask(addAction, addGenre));
+  displayTaskList();
 }
 
 displayTaskList();
 const inputAction = prompt('タスクを入力して下さい');
 const inputGenre = prompt('ジャンルを入力して下さい');
 addTask(inputAction, inputGenre);
-displayTaskList();
 alert('新しいタスク追加しました。');
 const nextAction = prompt('「確認、追加、削除、終了」の４つのいずれかを入力してください');
